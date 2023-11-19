8035cde5b0aac7dbc59217fc719ca59f8de35b60
Miguel Ángel Mora <miguel.mora@dart.technology>
2023-11-19 19:24:45 +0100

    añadido composable para sincronizar con firestore

--- composables/useFirestoreCollection.js
+++ composables/useFirestoreCollection.js
@@ -1,35 +1,46 @@
-import {getDB} from "~/services/fireinit";
-import {onSnapshot, collection, addDoc, doc, updateDoc} from 'firebase/firestore'
+import {
+  onSnapshot,
+  collection,
+  addDoc,
+  doc,
+  updateDoc,
+} from 'firebase/firestore'
+import { getDB } from '~/services/fireinit'
 export const useFirestoreCollection = () => {
   const path = ref('')
   const documents = ref({})
   const listener = ref()
   const error = ref('')
 
-  function unsubscribe(){
+  const ids = computed(() => Object.keys(documents.value))
+
+  const docsArray = computed(() =>
+    ids.value.map((id) => ({ id, ...documents.value[id] }))
+  )
+  function unsubscribe() {
     if (listener.value) {
-        listener.value()
-        listener.value = undefined
-        documents.value = {}
-        path.value = ''
-        error.value = ''
+      listener.value()
+      listener.value = undefined
+      documents.value = {}
+      path.value = ''
+      error.value = ''
     }
   }
-  function getCollection(){
+  function getCollection() {
     return collection(getDB(), path.value)
   }
   function onSnapshotChange(change) {
-    const {type, doc} = change
-    if (type === "added" || type === "modified") {
+    const { type, doc } = change
+    if (type === 'added' || type === 'modified') {
       documents.value[doc.id] = doc.data()
     }
-    if (type === "removed") {
+    if (type === 'removed') {
       delete documents.value[doc.id]
     }
   }
   function subscribe(newPath) {
     // si ha cambiado hay que dejar de escuchar primero
-    if (path.value!==newPath){
+    if (path.value !== newPath) {
       unsubscribe()
       clearError()
       path.value = newPath
@@ -37,12 +48,13 @@ export const useFirestoreCollection = () => {
         listener.value = onSnapshot(
           getCollection(),
           (snapshot) => {
-            snapshot.docChanges().forEach(onSnapshotChange);
+            snapshot.docChanges().forEach(onSnapshotChange)
           },
           (err) => {
             error.value = err.message
             // err.code contiene los códigos de error: https://github.com/grpc/grpc/blob/master/doc/statuscodes.md
-          })
+          }
+        )
       }
     }
   }
@@ -55,18 +67,30 @@ export const useFirestoreCollection = () => {
    * @param d document
    * @returns {Promise<string>} Promise with the id
    */
-  async function add(d){
+  async function add(d) {
     const docRef = await addDoc(getCollection(), d)
     return docRef.id
   }
 
-  function getDocRef(id){
+  function getDocRef(id) {
     return doc(getCollection(), id)
   }
 
-  async function update(id, changes){
+  async function update(id, changes) {
     await updateDoc(getDocRef(id), changes)
   }
 
-  return { ids, loading, toLoad, error, items, setIds, clearError }
+  return {
+    path,
+    documents,
+    listener,
+    error,
+    ids,
+    docsArray,
+    unsubscribe,
+    subscribe,
+    clearError,
+    add,
+    update,
+  }
 }
